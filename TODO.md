f12d186283884077b0fa6ec12aaa64a5f76fcbe0
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-04-02 22:58:27 -0400

    better responsive design for mobile view

---
+++ TODO.md
@@ -0,0 +1,18 @@
+# Project
+
+Project Description
+
+<em>[TODO.md spec & Kanban Board](https://bit.ly/3fCwKfM)</em>
+
+### Todo
+
+- [ ]   
+
+### In Progress
+
+- [ ] Nav click, scroll down [user has to know page changed]  
+
+### Done ✓
+
+- [x] Mobile screen width  
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
             <div className="home data">
             <div className="txt">
                 <h1 className='title title__md'>Welcome</h1>
-                <p className='para intro'>Look around and use these links for your web projects. These websites/youtube videos have been my go to resources for over two years and they keep serving me to this day. Use them and make your life easier.</p>
+                <p className='para intro'>Look around and use these links for your web projects. These websites/youtube videos have been my go to resources for over two years and they keep serving me to this day. Use them and make your coding life easier.</p>
             </div>
             <div className="cards">
                 <div className="flex__child card name title__xmd title"><span className="title__md">15 +</span> <span className='para_2'>content resources</span></div>
