8f714a849616cb5afbbda2946f1b7478250a4113
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-04-15 23:57:31 -0400

    UX | scroll down onPage load

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,96 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 
-// import LinkCard from './components/learning/learning_card'
-// import LearningCont from './components/learning/learning_cont'
-import Header from './pages/Header'
-import Home from './pages/Home'
-import Footer from './pages/Footer'
-import CardsContainer from './pages/CardsContainer'
+import Header from "./pages/Header";
+import Home from "./pages/Home";
+import Footer from "./pages/Footer";
+import CardsContainer from "./pages/CardsContainer";
 
-import './App.css'
+import "./App.css";
 
-import links from './assets/data/links.json'
-import Loading from './pages/Loading';
+import links from "./assets/data/links.json";
+import Loading from "./pages/Loading";
 function App() {
-  const [data, setData] = useState(links)
+  const [data, setData] = useState(links);
 
-  const [isLoading, setLoading] = useState(false)
-  useEffect(() => {
-    setLoading((true))
-    setTimeout(() => {
-      setLoading((false))
-    }, 1500)
-  }, [])
+  const [isLoading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   setLoading((true))
+  //   setTimeout(() => {
+  //     setLoading((false))
+  //   }, 1500)
+  // }, [])
+  // function pointDown() {
+  // }
 
   function handleClick() {
-    setLoading((true))
+    setLoading(true);
     setTimeout(() => {
-      setLoading((false))
-    }, 1500)
+      setLoading(false);
+    }, 1500);
   }
 
   return (
     <div className="App">
-        {isLoading && <Loading />}
-        <Router>
-          <Header func={handleClick} />
-          <Routes>
-              <Route path="*" element={<Home/>}/>  
-              <Route path="/" element={<Home/>}/>  
-              <Route path="home" element={<Home />} />  
-              <Route path="advice" element={<CardsContainer name="advice" data={data.advice}/>}/>  
-              <Route path="learning" element={<CardsContainer name="learning" data={data.learning_res}/>} />
-              <Route path="content" element={<CardsContainer name="content" data={data.content_res}/>} />
-              <Route path="css_simp" element={<CardsContainer name="css_simplifier" data={data.css_simplifiers}/>} />
-              <Route path="extensions" element={<CardsContainer name="extensions" data={data.extensions}/>} />
-              <Route path="docs" element={<CardsContainer name="docs" data={data.documentation}/>} />
-              <Route path="tools" element={<CardsContainer name="tools" data={data.tools}/>} />
-              <Route path="others" element={<CardsContainer name="others" data={data.others}/>} />
-          </Routes>
-          <Footer />
+      {isLoading && <Loading />}
+      <Router>
+        <Header func={handleClick} />
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route
+            path="advice"
+            element={<CardsContainer name="advice" data={data.advice} />}
+          />
+          <Route
+            path="learning"
+            element={
+              <CardsContainer name="learning" data={data.learning_res} />
+            }
+          />
+          <Route
+            path="content"
+            element={<CardsContainer name="content" data={data.content_res} />}
+          />
+          <Route
+            path="css_simp"
+            element={
+              <CardsContainer
+                name="css_simplifier"
+                data={data.css_simplifiers}
+              />
+            }
+          />
+          <Route
+            path="extensions"
+            element={
+              <CardsContainer name="extensions" data={data.extensions} />
+            }
+          />
+          <Route
+            path="docs"
+            element={<CardsContainer name="docs" data={data.documentation} />}
+          />
+          <Route
+            path="tools"
+            element={<CardsContainer name="tools" data={data.tools} />}
+          />
+          <Route
+            path="others"
+            element={<CardsContainer name="others" data={data.others} />}
+          />
+        </Routes>
+        <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,64 +1,194 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../assets/imgs/logo.svg";
 
-function NavBar({func}) {
-    function setColor() {
-        document.querySelectorAll('.nav__link').forEach(link => {
-            link.style.color = link.dataset.clr
-            link.style.borderColor = link.dataset.clr
-        })
-    }
-    useEffect(() => {
-        setColor()
-    }, [])
+function NavBar({ func }) {
+  function setColor() {
+    document.querySelectorAll(".nav__link").forEach((link) => {
+      link.style.color = link.dataset.clr;
+      link.style.borderColor = link.dataset.clr;
 
-    return ( 
-        <div className="header__cont">
-            <div className="header__top">
-                <div className="logo"><Logo /> </div>
-                <a href='https://resume-lul.netlify.app/' target="_blank" data-clr='#E60866' onLoad={(navData) => {return navData.isActive = true}} className="nav__link"> 
-                    <i className="fa-solid fa-rocket"></i> /resume
-                </a>
-                <NavLink id='advice' onClick={func} to='/advice' data-clr='#0a66c2' onLoad={(navData) => {return navData.isActive = true}} className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}> 
-                    <i className="fa-solid fa-rocket"></i> /advice
-                </NavLink>
-            </div>
-            <header className="header">
-                <div className="title title__big">WEB - DEV Resources</div>
-                <p className="para">Resources to make your coding life easier</p>
-            </header>
-            <nav className="nav">
-                <ul className="nav__links">
-                    <NavLink onClick={func} to='/' data-clr='#7c3aed' onLoad={(navData) => {return navData.isActive = true}} className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}> 
-                        <i className="fa-solid fa-house"></i> /home
-                    </NavLink>
-                    <NavLink onClick={func} to='/learning' data-clr='#0a66c2' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                        <i className="fa-brands fa-leanpub"></i>/Learning
-                    </NavLink>
-                    <NavLink onClick={func} to='/content' data-clr='#333' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                            <i className="fa-solid fa-compact-disc"></i>/Content_res
-                    </NavLink>
-                    <NavLink onClick={func} to='/css_simp' data-clr='#E60866' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                        <i className="fa-solid fa-wand-magic-sparkles"></i>/Css_simplifiers
-                    </NavLink>
-                    <NavLink onClick={func} to='/extensions' data-clr='#7c3aed' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                            <i className="fa-solid fa-compact-disc"></i>/Extensions
-                    </NavLink>
-                    <NavLink onClick={func} to='/docs' data-clr='#0a66c2' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                            <i className="fa-solid fa-compact-disc"></i>/documentation
-                    </NavLink>
-                    <NavLink onClick={func} to='/tools' data-clr='#333' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                            <i className="fa-solid fa-compact-disc"></i>/tools
-                    </NavLink>
-                    <NavLink onClick={func} to='/others' data-clr='#E60866' className={(navData) => (navData.isActive ? 'selected nav__link' : 'nav__link')}>
-                            <i className="fa-solid fa-compact-disc"></i>/others
-                    </NavLink>
-                </ul>
-            </nav>
+      //   link.addEventListener("click", () => {});
+    });
+  }
+
+  function scrollDown() {
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.querySelector(".header__cont").offsetHeight + 65,
+        behavior: "smooth",
+      });
+    }, 1550);
+  }
+  useEffect(() => {
+    setColor();
+  }, []);
+
+  return (
+    <div className="header__cont">
+      <div className="header__top">
+        <div className="logo">
+          <Logo />{" "}
         </div>
-     );
+        <a
+          href="https://resume-lul.netlify.app/"
+          target="_blank"
+          data-clr="#E60866"
+          onLoad={(navData) => {
+            return (navData.isActive = true);
+          }}
+          className="nav__link"
+        >
+          <i className="fa-solid fa-rocket"></i> /resume
+        </a>
+        <NavLink
+          id="advice"
+          onClick={() => {
+            func();
+            scrollDown();
+          }}
+          to="/advice"
+          data-clr="#0a66c2"
+          onLoad={(navData) => {
+            return (navData.isActive = true);
+          }}
+          className={(navData) =>
+            navData.isActive ? "selected nav__link" : "nav__link"
+          }
+        >
+          <i className="fa-solid fa-rocket"></i> /advice
+        </NavLink>
+      </div>
+      <header className="header">
+        <div className="title title__big">WEB - DEV Resources</div>
+        <p className="para">Resources to make your coding life easier</p>
+      </header>
+      <nav className="nav">
+        <ul className="nav__links">
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/"
+            data-clr="#7c3aed"
+            onLoad={(navData) => {
+              return (navData.isActive = true);
+            }}
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-house"></i> /home
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/learning"
+            data-clr="#0a66c2"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-brands fa-leanpub"></i>/Learning
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/content"
+            data-clr="#333"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-compact-disc"></i>/Content_res
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/css_simp"
+            data-clr="#E60866"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-wand-magic-sparkles"></i>/Css_simplifiers
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/extensions"
+            data-clr="#7c3aed"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-compact-disc"></i>/Extensions
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/docs"
+            data-clr="#0a66c2"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-compact-disc"></i>/documentation
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/tools"
+            data-clr="#333"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-compact-disc"></i>/tools
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              func();
+              scrollDown();
+            }}
+            to="/others"
+            data-clr="#E60866"
+            className={(navData) =>
+              navData.isActive ? "selected nav__link" : "nav__link"
+            }
+          >
+            <i className="fa-solid fa-compact-disc"></i>/others
+          </NavLink>
+        </ul>
+      </nav>
+      <div className="point__down">
+        <a
+          onClick={() => {
+            window.scrollTo({
+              top: document.querySelector(".header__cont").offsetHeight + 65,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <span></span>Scroll
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
